64bff197ee2667b11790aa7af5e2ca0da3b4a44b
Fr1x3 <gathufx@gmail.com>
2024-02-14 16:01:45 +0300

    creates button component

--- age-calculator-app/src/components/Button.jsx
+++ age-calculator-app/src/components/Button.jsx
@@ -1,6 +1,12 @@
+import ArrowIcon from '../assets/images/icon-arrow.svg'
 function Button(){
     return (
-        <div>buttom</div>
+        <div className="w-full relative">
+            <div className="h-[2px] w-full bg-light-grey absolute top-[calc(50%_-_1px)]"/>
+            <button className="h-10 w-10 rounded-full bg-purple flex justify-center items-center right-[calc(50%_-_1.25rem)] absolute z-10">
+                <img src={ArrowIcon} alt="arrow down icon" className='h-1/2 w-1/2'/>
+            </button>
+        </div>
     )
 }
 
