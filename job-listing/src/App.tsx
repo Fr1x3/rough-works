0d2aaab53d06ef75ab39616924bc02c5b5b863d2
Fr1x3 <gathufx@gmail.com>
2024-02-16 09:41:41 +0300

    added project assets

--- job-listing/src/App.tsx
+++ job-listing/src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-   <div className="text-4xl font-bold h-screen bg-red-400"> heello there</div>
+   <div className="text-4xl font-semibold h-screen bg-red-400"> heello there</div>
   )
 }
 
